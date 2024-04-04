d0b662290518df10593e81be4355f4a366dd603b
Gabriel Larramendi <gab.larramendi@gmail.com>
2024-04-04 11:47:37 -0300

    Adding footer and header in listView

--- app/src/main/java/dominando/android/adaptercostumizado/activities/CarInformationActivity.kt
+++ app/src/main/java/dominando/android/adaptercostumizado/activities/CarInformationActivity.kt
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import dominando.android.adaptercostumizado.R
+import dominando.android.adaptercostumizado.model.Vehicle
 
 class CarInformationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car_information)
 
+        val vehicle = intent.getSerializableExtra("vehicle") as Vehicle
         val txtModel = findViewById<TextView>(R.id.textViewModel)
         val txtYear = findViewById<TextView>(R.id.textViewYear)
 
-        txtModel.text = intent.getStringExtra("model")
-        txtYear.text = intent.getStringExtra("year")
+        txtModel.text = vehicle.model
+        txtYear.text = vehicle.year.toString()
     }
 }
--- app/src/main/java/dominando/android/adaptercostumizado/activities/MainActivity.kt
+++ app/src/main/java/dominando/android/adaptercostumizado/activities/MainActivity.kt
@@ -1,14 +1,20 @@
 package dominando.android.adaptercostumizado.activities
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.ListView
+import android.widget.TextView
 import android.widget.Toast
+import dominando.android.adaptercostumizado.R
 import dominando.android.adaptercostumizado.adapters.VehicleAdapter
 import dominando.android.adaptercostumizado.model.Vehicle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var txtFooter: TextView
 
     private val vehicles = mutableListOf(
         Vehicle("Onix", 2018, 1, true, true, false),
@@ -47,16 +53,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val listView = ListView(this)
         setContentView(listView)
+
         val adapter = VehicleAdapter(this, vehicles)
         listView.adapter = adapter
 
-        listView.setOnItemClickListener {parent, view, position, id ->
-            val (model, year) = vehicles[position]
-            Toast.makeText(this, "$model | $year | $position", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, CarInformationActivity::class.java)
-            intent.putExtra("model", model)
-            intent.putExtra("year", year.toString())
-            startActivity(intent)
+        initHeaderAndFooter(listView, adapter)
+
+        listView.setOnItemClickListener {parent, _, position, _ ->  //parent = ListView q dispara o evento, view q foi clicada, indice e id
+            val vehicle = parent.getItemAtPosition(position) as? Vehicle
+            if (vehicle != null) {
+                val p = position -1
+                val (model, year) = vehicle
+                Toast.makeText(this, "$model | $year | $p", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, CarInformationActivity::class.java)
+                intent.putExtra("vehicle", vehicle)
+                startActivity(intent)
+            }
         }
+    }
+
+    private fun initHeaderAndFooter(listView: ListView, adapter: VehicleAdapter) {
+        val padding = 8
+        val txtHeader = TextView(this)
+        txtHeader.setBackgroundColor(Color.GRAY)
+        txtHeader.setTextColor(Color.WHITE)
+        txtHeader.setText(R.string.header_text)
+        txtHeader.setPadding(padding, padding, 0, padding)
+        listView.addHeaderView(txtHeader)
+
+        txtFooter = TextView(this)
+        txtFooter.text = resources.getQuantityString(R.plurals.footer_text, adapter.count, adapter.count)
+        txtFooter.setBackgroundColor(Color.LTGRAY)
+        txtFooter.setTextColor(Color.BLACK)
+        txtFooter.gravity = Gravity.END
+        txtFooter.setPadding(0, padding, padding, padding)
+        listView.addFooterView(txtFooter)
     }
 }
